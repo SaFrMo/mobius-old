64a6007eb1721f5ed9f1167a8257de37c025dd3e
Sander <sandermoolin@gmail.com>
2017-04-22 00:28:20 -0400

    Basic platforming and map setup complete

--- src/states/Game.js
+++ src/states/Game.js
@@ -3,25 +3,31 @@ import Phaser from 'phaser'
 import Player from '../gameobjects/Player'
 
 export default class extends Phaser.State {
-  init () {}
-  preload () {}
+    init () {}
+    preload () {}
 
-  create () {
+    create () {
 
-      this.objects = {}
+        // Set up environment
+        this.map = this.game.add.tilemap('map')
+        this.map.addTilesetImage('ground_1x1')
+        this.map.setCollisionBetween(1, 2)
+        this.blocking = this.map.createLayer('Blocks')
+        this.blocking.resizeWorld()
 
-      // Set up platformer physics
-      this.game.physics.startSystem(Phaser.Physics.ARCADE)
-      this.game.physics.arcade.gravity.y = 500
+        // Set up platformer physics
+        this.game.physics.startSystem(Phaser.Physics.ARCADE)
+        this.game.physics.arcade.gravity.y = 500
 
-      // Set up player sprite
-      this.objects.player = this.game.add.existing(
-          new Player( this.game, 0, 0, 'player' ) )
+        // Set up player sprite
+        this.player = this.game.add.existing(
+            new Player( this.game, 0, 0, 'player' )
+        )
 
+    }
 
-  }
-
-  render () {
-
-  }
+    update(){
+        // Register collisions
+        this.game.physics.arcade.collide(this.player, this.blocking)
+    }
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -2,21 +2,23 @@ import Phaser from 'phaser'
 import { centerGameObjects } from '../utils'
 
 export default class extends Phaser.State {
-  init () {}
+    init () {}
 
-  preload () {
-    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
-    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-    centerGameObjects([this.loaderBg, this.loaderBar])
+    preload () {
+        this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
+        this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
+        centerGameObjects([this.loaderBg, this.loaderBar])
 
-    this.load.setPreloadSprite(this.loaderBar)
-    //
-    // load your assets
-    //
-    this.load.image('player', 'assets/images/player.png')
-  }
+        this.load.setPreloadSprite(this.loaderBar)
+        //
+        // load your assets
+        //
+        this.load.image('player', 'assets/images/player.png')
+        this.load.image('ground_1x1', 'assets/images/ground_1x1.png')
+        this.load.tilemap('map', 'assets/tilemaps/map.json', null, Phaser.Tilemap.TILED_JSON)
+    }
 
-  create () {
-    this.state.start('Game')
-  }
+    create () {
+        this.state.start('Game')
+    }
 }
